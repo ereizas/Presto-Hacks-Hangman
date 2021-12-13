ce1e9c18a915be5e5a03d76da1e8f286cfff7944
ereizas <71237683+ereizas@users.noreply.github.com>
2021-12-13 17:55:49 -0500

    Add files via upload

--- Hangman test.py
+++ Hangman test.py
@@ -2,10 +2,8 @@ import pygame
 import time
 import random
 from pygame.locals import *
-# The Design of the Game
-# when the user reveil two letters will give them a bonus of one letter
+from pygame.event import wait
 
-# import pygame.locals for some interaction with key coordinates
 from pygame.locals import (
 	K_ESCAPE,
 	QUIT,
@@ -24,30 +22,14 @@ SCREEN_HEIGHT = 600
 # Create a screen object
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 # Drawing on The Scrren
-
 screen.fill((0,0,0)) # change the background to black
 
-
-
-# .blit => block transfer (it blit a surface into another surface)
-
-
-
-# Create a sprite object for Player (not sure if needed)
-class Player(pygame.sprite.Sprite):
-	def __init__(self):
-		super(Player, self).__init__()
-		self.surface = pygame.Surface((75,25))
-		self.surface.fill((202,228,241))
-		self.rect = self.surface.get_rect()
-# Create a new instance of player
-player = Player()
 
 #### Creating The Game loop
 # => frames => stops the frames when the player is loss (the human are k¡lled) and when the player closes the Window
 # variable to keep the main loop Running
 running = True
-font = pygame.font.SysFont("helvetica",5) #or verdana
+font = pygame.font.SysFont("helvetica",30) #or verdana
 
 pygame.display.flip()
 p1score = 0
@@ -70,56 +52,41 @@ computerWords = [#artists
 # Main loop
 while running:
 	pygame.display.set_caption("Hangmoo")
-	font = pygame.font.SysFont("helvetica",30) #or verdana
-	screen.fill((250,0,0))
-	screen.blit(font.render("Hello",True,(250,250,250)),(300,20))
-	# Look  at every event in the queue and start the event handler
-	for event in pygame.event.get(): # pygame.event it has all the events associated with it
-		# check if the player hit the key ?
-		if event.type == KEYDOWN:
-			# check if the key was K_ESCAPE
-			if event.key == K_ESCAPE:
-				running = False
-		
-		if event.type == pygame.QUIT: 
-			break
-
-		# Check if the user click into the Close Button ? if so , stop the loop
-		elif event.type == QUIT:
-			running = False
 	
-
-	##player_1 = input("Enter the name of the player 1:")##
-	
-
-	hangman = {0:pygame.draw.line(screen,(150,75,0),(200,350),(400,350),10), 1:pygame.draw.line(screen,(150,75,0),(300,350),(300,100),10),2:pygame.draw.line(screen,(150,75,0),(300,100),(400,100),10),
-	3:pygame.draw.line(screen,(150,75,0),(400,100),(400,150),10),4:pygame.draw.circle(screen,(250,250,250),(400,180),30,5),5:pygame.draw.line(screen,(250,250,250),(400,210),(400,270),5),6:pygame.draw.line(screen,(250,250,250),
-	(400,240),(370,210),5), 7:pygame.draw.line(screen,(250,250,250),(400,240),(430,210),5), 8:pygame.draw.line(screen,(250,250,250),(400,270),(370,300),5),9:pygame.draw.line(screen,(250,250,250),(400,270),(430,300),5)}
-	##^
-
-	
-
-	##print(F"#{player_1} Score => {str(p1score)} ")##
+	screen.fill((0,0,0))
+	screen.blit(font.render("Player Score: " + str(p1score),True, (255,255,255)),(10,50))
+	screen.blit(font.render("Computer Score: " + str(compScore),True, (255,255,255)),(500,50))
+	hangman = {0: lambda: pygame.draw.line(screen, (150, 75, 0), (200, 350), (400, 350), 10), 1: lambda: pygame.draw.line(screen, (150, 75, 0), (300, 350), (300, 100), 10), 2: lambda: pygame.draw.line(screen, (150, 75, 0), (300, 100), (400, 100), 10),
+            3: lambda: pygame.draw.line(screen, (150, 75, 0), (400, 100), (400, 150), 10), 4: lambda: pygame.draw.circle(screen, (250, 250, 250), (400, 180), 30, 5), 5: lambda: pygame.draw.line(screen, (250, 250, 250), (400, 210), (400, 270), 5), 6: lambda: pygame.draw.line(screen, (250, 250, 250),
+			(400, 240), (370, 210), 5), 7: lambda: pygame.draw.line(screen, (250, 250, 250), (400, 240), (430, 210), 5), 8: lambda: pygame.draw.line(screen, (250, 250, 250), (400, 270), (370, 300), 5), 9: lambda: pygame.draw.line(screen, (250, 250, 250), (400, 270), (430, 300), 5)}
 	answer = []
 	guess = []
+	incorrGuesses = []
 	hangindex = -1
 	correct = False
 	def arrToStr(arr):
 		res = ""
 		for i in arr:
-			res+=i
+			res+=i+" "
 		return res
-	
-
 
 	#makes sure the randomly generated category arrays are not empty
 	randInd1 = random.randint(0,3)
 	while computerWords[randInd1]==[]:
 		randInd1 = random.randit(0,3)
 	randInd2 = random.randint(0,len(computerWords[randInd1])-1)
-	print(computerWords[randInd1][randInd2])
+	##print(computerWords[randInd1][randInd2])
+	if(randInd1==0):
+		screen.blit(font.render("Artists",True,(255,255,255)),(300,10))
+	elif(randInd1==1):
+		screen.blit(font.render("Instruments",True,(255,255,255)),(300,10))
+	elif(randInd1==2):
+		screen.blit(font.render("Genres",True,(255,255,255)),(300,10))
+	elif(randInd1==3):
+		screen.blit(font.render("Musical Terms",True,(255,255,255)),(300,10))
+
 	for char in computerWords[randInd1][randInd2]:
-		answer.append(char)
+		answer.append(char.lower())
 		if char != " ":
 			guess.append("_")
 		elif char == " ":
@@ -127,17 +94,22 @@ while running:
 	computerWords[randInd1].remove(computerWords[randInd1][randInd2])
 	
 	guessStr = arrToStr(guess)
-	output = font.render(guessStr,True,(255,255,255))
+	
 	pygame.draw.rect(screen,(0,0,0),pygame.Rect(0,375,800,225))
+	output = font.render(guessStr,True,(255,255,255))
 	screen.blit(output,(200,400))
+	pygame.display.update()
 
 	while not correct:
 		letter_guess = ""
 		while letter_guess == "" or len(letter_guess)>1 or not letter_guess.isalpha():
 			for event in pygame.event.get():
 				if event.type == pygame.KEYDOWN:
+					if pygame.key.name(event.key) == "escape":
+						pygame.quit()
 					letter_guess = pygame.key.name(event.key)
-					print(letter_guess)
+		
+				
 		if letter_guess in answer:
 			index = 0
 			for char in answer:
@@ -146,36 +118,48 @@ while running:
 				index+=1
 
 			guessStr = arrToStr(guess)
-			output = font.render(guessStr,True,(0,0,0))
+			
 			pygame.draw.rect(screen,(0,0,0),pygame.Rect(0,375,800,225))
+			output = font.render(guessStr,True,(255,255,255))
 			screen.blit(output,(200,400))
+			pygame.display.update()
 
 			if guess == answer:
 
 				guessStr = arrToStr(guess)
-				output = font.render(guessStr,True,(0,0,0))
+
 				pygame.draw.rect(screen,(0,0,0),pygame.Rect(0,375,800,225))
+				output = font.render(guessStr,True,(255,255,255))
 				screen.blit(output,(200,400))
+				pygame.display.update()
+				pygame.time.wait(2000)
 
 				p1score+=1
 				screen.fill((0,0,0))
 				correct=True
 		else:
-			print("+1")
 			hangindex+=1
-			if hangindex == 0:
-				pygame.draw.line(screen,(150,75,0),(200,350),(400,350),10)
-				print("hanging")
-		##this ends program if user is wrong
-		if hangindex == 9:
-			print("done")
-			compScore+=1
-			screen.fill((0,0,0))
-			break
+			incorrGuesses.append(letter_guess)
+			guessed = font.render(arrToStr(incorrGuesses),True,(255,255,255))
+			screen.blit(font.render("Incorrect Guesses:",True,(255,255,255)),(470,160))
+			screen.blit(guessed,(470,200))
+			f = hangman.get(hangindex)
+			#call function:
+			f()
+			pygame.display.update()
+			if hangindex == 9:
+				compScore+=1
+				f()
+				pygame.time.wait(2000)
+				screen.fill((0,0,0))
+				pygame.display.update()
+				break
 
 	guessStr = arrToStr(guess)
-	output = font.render(guessStr,True,(0,0,0))
+	
 	pygame.draw.rect(screen,(0,0,0),pygame.Rect(0,375,800,225))
+	output = font.render(guessStr,True,(255,255,255))
 	screen.blit(output,(200,400))
+	pygame.display.update()
 
 pygame.quit()
